f49494597ac0b946c30a336ca3199404e9054562
Norbert Sliva <noroslivka@gmail.com>
2018-03-28 02:59:26 +0200

    Winstart - add help and comments

--- winstart.bash
+++ winstart.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
-
+#
+# Opens the specified files in windows.
 function winstart() {
+  if [ "$#" -eq "0" ]; then
+    echo "Usage: [file...]" 1>&2;
+    return;
+  fi
   local call
-  for file in "$@"
-  do
+  for file in "$@"; do
     call+="start \"\" \"${file}\"\n"
   done
   echo -e "$call" | cmd.exe >2 /dev/null
