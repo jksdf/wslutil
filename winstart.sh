2a35d084f06cd6945c96e9ef9543158db193fb6d
Norbert Slivka <noroslivka@gmail.com>
2018-03-30 22:01:49 +0200

    Fixed winstart for folders starting with "n".

--- winstart.sh
+++ winstart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
 # Opens the specified files in windows.
+THIS=`readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0`
+source "$(dirname $THIS)/winpath.sh"
+
 function winstart() {
   if [ "$#" -eq "0" ]; then
     echo "Usage: [file...]" 1>&2;
@@ -9,7 +12,7 @@ function winstart() {
   local call
   local winfile
   for file in "$@"; do
-    winfile=$(winpath "$file")
+    winfile=$(winpath "$file" | sed "s/\\\\/\\\\\\\\/g")
     call+="start \"\" \"$winfile\"\n"
   done
   echo -e "$call" | cmd.exe > /dev/null
