0eda5a9f7458f4172f6a951fdfc01b4cd5235f6f
jksdf <noroslivka@gmail.com>
2018-04-02 04:29:40 +0200

    wsl-notify-send fixes

--- all.sh
+++ all.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-THIS=`readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0`
-cd $(dirname $THIS)
-
+__WSL_TMP=$(pwd)
+__WSL_THIS=`readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0`
+cd $(dirname $__WSL_THIS)
 source ./winstart.sh
 source ./winpath.sh
+source ./wsl-notify-send.sh
+
+cd "$__WSL_TMP"
 
--- wsl-notify-send.sh
+++ wsl-notify-send.sh
@@ -1,69 +1,100 @@
 #!/bin/bash
-function wsl-notify-send() {
+
+__WSL_TMP=$(pwd)
+__WSL_THIS=`readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0`
+cd $(dirname $__WSL_THIS)
+source ./winpath.sh
+cd "$__WSL_TMP"
+
+wsl-notify-send() {
+  
+  ISVAL=0
+  VALNAME=""
 
   while [ "$#" -ne "0" ]; do
-    PARAM=$(echo $1 | awk -F= '{print $1}')
-    if echo "$!" | grep -q "="; then
-      VALUE=$(echo $1 | awk -F= '{print $2}')
+    if [ "$ISVAL" -eq "1" ]; then
+      if echo "$1" | grep -q "="; then
+        VALUE=$(echo $1 | awk -F= '{print $2}')
+      else
+        VALUE=$1
+        shift
+      fi
+      #echo "setting $VALNAME to $VALUE"
+      declare $VALNAME=$VALUE
+      VALNAME=""
+      ISVAL=0
     else
-      shift
-      VALUE=$($1)
+      EQSPLIT=0
+      if echo "$1" | grep -q "="; then
+        EQSPLIT=1
+      fi
+      PARAM=$(echo $1 | awk -F= '{print $1}')
+      case $PARAM in
+        -hh | --help)
+          echo "See 'man'."
+          return 1
+          ;;
+        -u | --urgency)
+          ISVAL=1
+          VALNAME="URGENCY"
+          ;;
+        -t | --expire-time)
+          ISVAL=1
+          VALNAME="EXPIRE"
+          ;;
+        -i | --icon)
+          ISVAL=1
+          VALNAME="ICON"
+          ;;
+        -c | --category)
+          ISVAL=1
+          VALNAME="CATEGORY"
+          ;;
+        -h | --hint)
+          ISVAL=1
+          VALNAME="HINT"
+          ;;
+        *)
+          break
+          ;;
+      esac
+      if [ $EQSPLIT -eq "0" ]; then
+        shift
+      fi
     fi
-    
-    case $PARAM in
-      -? | --help)
-        echo "See 'man'."
-        exit
-        ;;
-      -u | --urgency)
-        URGENCY="$VALUE"
-        ;;
-      -t | --expire-time)
-        EXPIRE="$VALUE"
-        ;;
-      -i | --icon)
-        ICON="$VALUE"
-        ;;
-      -c | --category)
-        CATEGORY="$VALUE"
-        ;;
-      -h | --hint)
-        HINT=$VALUE
-        ;;
-      *)
-        break
-        ;;
-    esac
-    shift
   done
 
   DICON="icons/info.png"
-  if [ "$URGENCY" -eq "critical" ]; then
+  if [ "0$URGENCY" == "0critical" ]; then
     DICON="icons/error.png"
   fi
+
+  if [ "x$URGENCY" == "xinfo" ]; then
+    SOUND="-Silent"
+  fi
   
-  if [ "$EXPIRE" -ne "" ]; then
+  if [ "x$EXPIRE" != "x" ]; then
     echo "Expire time does not work in this implementation." 1>&2
   fi
 
-  if [ "$ICON" -ne "" ]; then
+  if [ "x$ICON" != "x" ]; then
     DICON="$ICON"
   fi
-
   
-  
-  if [ "$#" -eq "0" ]; then
+  if [ "$#" == "0" ]; then
     echo "Set params"
     return 1
   fi
   
   local REQUEST=""
-
   while [ "$#" -ne "1" ]; do
     REQUEST+="\"$1\", "
     shift
   done 
   REQUEST+=\"$1\"
   
-  powershell.exe -ExecutionPolicy Bypass -NoLogo -NoProfile -NonInteractive -COMMAND "Import-Module BurntToast; New-BurntToastNotification -Text $REQUEST"
+  DICON=$(winpath "$DICON")
+
+  #echo "Import-Module BurntToast; New-BurntToastNotification -AppLogo \"$DICON\" $SOUND -Text $REQUEST"
+  powershell.exe -ExecutionPolicy Bypass -NoLogo -NoProfile -NonInteractive -COMMAND "Import-Module BurntToast; New-BurntToastNotification -AppLogo \"$DICON\" $SOUND -Text $REQUEST"
 }
